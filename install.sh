374f4ef4eed4a0e3f5c53004b668c172f3bee071
tristan-fraboulet <tristan.fraboulet@etu.isae-ensma.fr>
2026-07-17 13:52:23 -0300

    chore: test of node to evaluate cpu use

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ..
 
 # Clone TurtleBot3 repositories from source
 git clone -b noetic git@github.com:gustavorvillela/turtlebot3_diff.git
-
+git clone https://github.com/alspitz/cpu_monitor.git
 
 
 echo "TurtleBot3 packages cloned from source successfully."
