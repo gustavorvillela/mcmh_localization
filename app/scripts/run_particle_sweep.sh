211e1679975cc37a8b04532f283f583e04c512e5
tristan-fraboulet <tristan.fraboulet@etu.isae-ensma.fr>
2026-09-15 10:07:58 -0300

    fix: moved odom_t_esp to the right place

--- app/scripts/run_particle_sweep.sh
+++ app/scripts/run_particle_sweep.sh
@@ -12,7 +12,7 @@ RESULTS_DIR="$(rospack find mcmh_localization)/results"
 DEFAULT_BAG_DIR="$(rospack find mcmh_localization)/bags"
 PARAMS_DIR="$(rospack find mcmh_localization)/params"
 REPEATS=50   # number of repeats per configuration
-CLEAR=1   # Clean the results dir?
+CLEAR=0   # Clean the results dir?
 MODEL="turtlebot3_${TURTLEBOT3_MODEL:-waffle}"  # TurtleBot3 model (waffle or burger)
 mkdir -p "$RESULTS_DIR"
 
